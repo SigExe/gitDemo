ec726696d33a5181cdba9c74bde145042ea396c3
SigExe <SIG96@protonmail.com>
2020-08-01 10:54:28 -0500

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -4,5 +4,6 @@ public class HappyB20 {
 
         System.out.println("B20 is always happy");
         System.out.println("Gol");
+        System.out.println("I hope I have no more problems with git from now on");
     }
 }
--- src/HelloB20.java
+++ src/HelloB20.java
@@ -3,5 +3,8 @@ public class HelloB20 {
     public static void main(String[] args) {
 
         System.out.println("Hello, B20, it my pleasure to be with you");
+
+
+        System.out.println("Let's hope I have no problem with IntelliJ from now on");
     }
 }
